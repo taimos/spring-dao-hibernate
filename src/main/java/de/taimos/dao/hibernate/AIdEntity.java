eeb94ab1b40d7d7a0641eef7f67a954324d387be
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2013-08-13 01:13:39 +0200

    equals for AIdEntity

--- src/main/java/de/taimos/dao/hibernate/AIdEntity.java
+++ src/main/java/de/taimos/dao/hibernate/AIdEntity.java
@@ -34,4 +34,34 @@ public abstract class AIdEntity implements IEntity<Long> {
 		return this.id;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + ((this.id == null) ? 0 : this.id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		AIdEntity other = (AIdEntity) obj;
+		if (this.id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!this.id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+	
 }
